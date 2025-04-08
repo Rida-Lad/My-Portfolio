e85c57efe5dffd13c01d657037a99b0d668e6273
Rida-Lad <ridlad7@gmail.com>
2025-04-08 15:31:32 +0100

    update HeroSection component to enhance red glow effect and improve layout structure

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -4,34 +4,35 @@ export default function HeroSection() {
   return (
     <section className="bg-black text-white min-h-screen flex items-center py-20 px-6 md:px-12 relative overflow-hidden">
       {/* Red glow effect */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-red-900 opacity-20 blur-3xl rounded-full"></div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-red-900 opacity-40 blur-3xl rounded-full"></div>
 
-      <div className="max-w-6xl mx-auto w-full relative z-10">
+      <div className="max-w-6xl mx-auto w-full relative z-10 ">
         {/* Name and Title */}
-        <div className="mb-16">
-          <h1 className="text-5xl md:text-7xl font-bold mb-4">RIDA LADIB</h1>
-          <div className="flex items-center">
+        <div className="mb-30 text-center">
+          <h1 className="text-5xl md:text-7xl font-bold mb-4 block">RIDA LADIB</h1>
+          <div className="flex items-center justify-center">
             <div className="w-12 h-1 bg-red-900 mr-4"></div>
-            <p className="text-xl md:text-2xl">WEB DEVELOPER & DATA ANALYST</p>
+            <p className="text-xl md:text-2xl block">WEB DEVELOPER & DATA ANALYST</p>
           </div>
         </div>
 
+
         {/* Stats */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="border-t-2 border-red-900 pt-4">
-            <p className="text-4xl font-bold">3</p>
-            <p className="text-gray-400">certifications</p>
+        <div className="flex flex-row flex-wrap items-center justify-center gap-8">
+          <div className=" pl-4 text-center">
+            <p className="text-lg font-bold">3</p>
+            <p className="text-sm text-gray-400">Certifications</p>
           </div>
-          <div className="border-t-2 border-red-900 pt-4">
-            <p className="text-4xl font-bold">10</p>
-            <p className="text-gray-400">projects</p>
+          <div className="border-l-4 border-red-900 pl-4 text-center">
+            <p className="text-lg font-bold">10</p>
+            <p className="text-sm text-gray-400">Projects</p>
           </div>
-          <div className="border-t-2 border-red-900 pt-4">
-            <p className="text-4xl font-bold">20+</p>
-            <p className="text-gray-400">technologies</p>
+          <div className="border-l-4 border-red-900 pl-4 text-center">
+            <p className="text-lg font-bold">20+</p>
+            <p className="text-sm text-gray-400">Technologies</p>
           </div>
         </div>
       </div>
-    </section>
+    </section >
   );
 }
